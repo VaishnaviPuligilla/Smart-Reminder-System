485f1bfbcd33b516a4c3eff42d274d781a19382c
VaishnaviPuligilla <vaishnavi12puligilla@gmail.com>
2026-01-13 01:24:36 +0530

    Delete backend directory

--- backend/services/autoRefreshService.js
+++
@@ -1,9 +0,0 @@
-// services/autoRefreshService.js
-const cron = require('node-cron');
-
-// This service will auto-complete overdue reminders and refresh data
-const autoRefreshService = () => {
-  console.log('[AutoRefreshService] Started - checking for overdue reminders every minute');
-};
-
-module.exports = { autoRefreshService };
